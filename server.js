39cd43bd819948df646105918f58127f5b87f0ff
amaradouk10 <amaradouk10@gmail.com>
2021-07-23 20:12:04 +0000

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express= require('express');
 const app = express();
 const path= require('path');
-
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.static(path.join(__dirname,"views")));
 
 app.get('/',(request,response)=>{
-    response.sendFile(path.join(__dirname,"views","index.html"));
+    response.sendFile(path.join(__dirname,"views","index.html","help.html"));
 })
 
 app.post('/',(request,response)=>{
